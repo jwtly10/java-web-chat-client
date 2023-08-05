4159506e9c1a4e1cd5712b10b23a794d7e26f8e7
Josh Watley <watleyjosh@gmail.com>
2023-08-05 12:58:28 +0100

    Adding server

---
+++ src/main/java/com/jwtly10/ClientHandler.java
@@ -0,0 +1,16 @@
+package com.jwtly10;
+
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+
+    @Override
+    public void run() {
+
+        
+    }
+     
+    
+}
